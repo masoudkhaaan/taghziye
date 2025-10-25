2b0e4fb0402a4e3b48f2bb3b8ec7ed737b3d4980
masoudkhaan <masoudkhaan84@gmail.com>
2025-10-25 18:54:59 +0330

    add useCreateCategory & useUpdateCategory

--- src/app/(dashboard)/admin/foods-management/categories/_services/use-category-mutations.ts
+++ src/app/(dashboard)/admin/foods-management/categories/_services/use-category-mutations.ts
@@ -1,6 +1,37 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCategory } from "./categoryMutations";
 import { toast } from "sonner";
+import { CategorySchema } from "../_types/categorySchema";
+import { createCategory, updateCategory } from "./categoryQueries";
+
+const useCreateCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: CategorySchema) => {
+      await createCategory(data);
+    },
+    onSuccess: () => {
+      toast.success("دسته بندی با موفقیت ایجاد شد.");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+};
+
+const useUpdateCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: CategorySchema) => {
+      await updateCategory(data);
+    },
+    onSuccess: () => {
+      toast.success("دسته بندی با موفقیت به روز شد.");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+};
+
 
 const useDeleteCategory = () => {
     
@@ -17,4 +48,7 @@ const useDeleteCategory = () => {
   });
 };
 
-export { useDeleteCategory };
+
+
+
+export { useDeleteCategory,useUpdateCategory,useCreateCategory };
