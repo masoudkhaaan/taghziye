c529c3ecb7514e61cb176cdb30d69ba7aa6bf448
masoudkhaan <masoudkhaan84@gmail.com>
2025-10-30 17:47:54 +0330

    add ServingUnit

--- src/components/no-items-found.tsx
+++ src/components/no-items-found.tsx
@@ -13,12 +13,11 @@ const NoItemsFound = ({ OnClick }: noItemsFoundProps) => {
       <CircleOff className="text-primary mb-2" />
       <h3 className="text-lg font-medium">آیتمی یافت نشد</h3>
       <p className="text-foreground/60 mt-1 text-shadow-sm">
-        سعی کنید آیتم جدید اضافه کنید
-        <div>      <Button variant="outline" className="mt-4" onClick={OnClick}>
-          اضافه کردن آیتم جدید
-        </Button></div>
-  
+        سعی کنید آیتم جدید اضافه کنید{" "}
       </p>
+      <Button variant="outline" className="mt-4" onClick={OnClick}>
+        اضافه کردن آیتم جدید
+      </Button>
     </div>
   );
 };
