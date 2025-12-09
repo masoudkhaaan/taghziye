7163b643bce56daf0bda1639fabaec6eeeb5c23f
masoudkhaan <masoudkhaan84@gmail.com>
2025-12-09 13:23:02 +0330

    add defaultOptions of mutations

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,12 +5,28 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Toaster } from "./ui/sonner";
 import { AlertDialogProvider } from "./ui/alert-dialog-provider";
+import { toast } from "sonner";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError: (e) => {
+        if (e.message === "NEXT_REDIRECT") return;
+        toast.error(e.message || "خطایی رخ داد.");
+      },
+      onSuccess: () => {
+        toast.success("عملیات با موفقیت انجام شد.");
+      },
+    },
+  },
+});
+
 
 type ProvidersType = {
   children: ReactNode;
 };
+
+
 
 const Providers = ({ children }: ProvidersType) => {
   return (
