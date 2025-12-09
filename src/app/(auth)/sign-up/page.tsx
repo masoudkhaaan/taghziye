c9b79f11db0f995a1d6503a3cc3022453f1c8866
masoudkhaan <masoudkhaan84@gmail.com>
2025-12-09 13:21:13 +0330

    add sign-in page  & sign-up page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,19 @@
+import { Role } from "$/generated/prisma";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { SignUpForm } from "./_components/sign-up-form";
+
+const Page = async () => {
+  const session = await auth();
+  if (session?.user?.role === Role.ADMIN)
+    redirect("/admin/foods-management/foods");
+  if (session?.user?.role === Role.USER) redirect("/client");
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Page;
